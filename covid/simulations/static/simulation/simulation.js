91d1ff29c2dc33a92c7d7429aa5eab7db54f9986
Andrew3203 <kuand3203@gmail.com>
2020-04-02 22:24:38 +0300

    one arc moving

--- covid/simulations/static/simulation/simulation.js
+++ covid/simulations/static/simulation/simulation.js
@@ -1,4 +1,20 @@
 var canvas = document.getElementById('liner');
-var ctx = canvas.getClientRects('2d');
-
+var ctx = canvas.getContext('2d');
 var pi = Math.PI;
+
+var x = 100;
+var v = -1;
+function drawDot(){
+    ctx.clearRect(0,0, 800, 50);
+    ctx.beginPath();
+    ctx.fillStyle="pink";
+    ctx.arc(x, 25, 10, 0, 2*pi);
+    ctx.stroke();
+    ctx.fill();
+    timer = setTimeout(drawDot, 15);
+    x+=v;
+    if((x === 800 - 10) || (x === 10)){
+        v *= -1;
+    }
+}
+drawDot();
